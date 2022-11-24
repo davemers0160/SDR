af0b817a3c7f2fc49f35cde695e01ea632c67b39
Dave <davemers@iu.edu>
2022-11-24 01:50:26 -0500

    adding more functionality

--- hackrf/rx_example/src/hackrf_rx_main.cpp
+++ hackrf/rx_example/src/hackrf_rx_main.cpp
@@ -17,7 +17,7 @@
 #include <hackrf.h>
 
 // Custom Includes
-
+#include <hackrf_common.h>
 
 // Project Includes
 
@@ -37,25 +37,47 @@ int main(int argc, char** argv)
     hackrf_device_list_t *hackrf_list;
     hackrf_transfer hackrf_rx;
     hackrf_device* dev = NULL;
+
+    double sample_rate = 10000000;
+    uint64_t freq = 315000000;
     
-    
+    int32_t rv;
+
+    uint8_t board_id = 0;
+
+    try
+    {
+        rv = hackrf_init();
+
+        rv = select_hackf(&dev);
+        if (rv != HACKRF_SUCCESS)
+        {
+            std::cout << "error opening HackRF: " << std::string(hackrf_error_name((enum hackrf_error)rv)) << std::endl;
+        }
+
+        rv = hackrf_board_id_read(dev, &board_id);
+        if (rv == HACKRF_SUCCESS)
+        {
+            std::cout << "HackRF board ID: " << std::string(hackrf_board_id_name((enum hackrf_board_id)board_id)) << std::endl;
+        }
+        else
+        {
+            std::cout << "error getting board id: " << std::string(hackrf_error_name((enum hackrf_error)rv)) << std::endl;
+        }
+
+        rv = hackrf_set_sample_rate(dev, sample_rate);
+        rv = hackrf_set_freq(dev, freq);
+
+
+
+    }
+    catch (std::exception e)
+    {
+        std::cout << "error: " << e.what() << std::endl;
+    }
     // initialize the hackrf - required before opening
-    auto rv = hackrf_init();
     
-    
-    hackrf_list = hackrf_device_list();
-    
-    
-    rv = hackrf_device_list_open(hackrf_list, 0, &dev);
-    
-    // free the list 
-    hackrf_device_list_free(hackrf_list);
-    
-    
-    
-    rv = hackrf_close(dev);
-    
+    rv = hackrf_close(dev); 
     rv = hackrf_exit();
-    
     
 }   // end of main
