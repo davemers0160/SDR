058d1432a515fdb93d08d2df48bd6d8e9b0bd70b
Dave <davemers@iu.edu>
2022-12-04 22:13:45 -0500

    reworking rx to be unsigned

--- hackrf/rx_example/src/hackrf_rx_main.cpp
+++ hackrf/rx_example/src/hackrf_rx_main.cpp
@@ -2,12 +2,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
-
+#include <Windows.h>
 #elif defined(__linux__)
 
 #endif
 
 #include <cstdint>
+#include <ctime>
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -23,10 +24,23 @@
 
 // Project Includes
 
-const uint64_t block_size = 262144 >> 1;
-static std::vector<std::complex<int8_t>> samples;
+const uint64_t block_size = 262144;
+static std::vector<uint8_t> samples;
 
 static uint64_t blocks_captured;
+
+//-----------------------------------------------------------------------------
+inline void sleep_ms(uint32_t value)
+{
+
+#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+    Sleep(value);
+#else
+    const timespec delay[] = { 0, (uint32_t)(value * 1000000) };
+    nanosleep(delay, NULL);
+#endif
+
+}   // end of sleep_ms
 
 //-----------------------------------------------------------------------------
 int rx_callback(hackrf_transfer* transfer)
@@ -34,9 +48,10 @@ int rx_callback(hackrf_transfer* transfer)
     size_t samples_captured;
 
     ++blocks_captured;
-    samples_captured = transfer->valid_length>>1;
+    samples_captured = transfer->valid_length;
+    fprintf(stderr, "samples_captured: %d\n", samples_captured);
 
-    std::vector<std::complex<int8_t>> tmp(transfer->buffer, transfer->buffer + samples_captured);
+    std::vector<uint8_t> tmp(transfer->buffer, transfer->buffer + samples_captured);
 
     std::copy(tmp.begin(), tmp.end(), std::back_inserter(samples));
 
@@ -54,15 +69,16 @@ int main(int argc, char** argv)
     // hackrf specific structs
     hackrf_device* dev = NULL;
 
-    double sample_rate = 10000000;
-    uint64_t freq = 315000000;
-    
+    double sample_rate = 8000000;
+    uint64_t freq = 314500000;
+    uint32_t rx_gain = 20;
+
     int32_t rv;
 
     uint8_t board_id = 0;
 
     // determine how many blocks to capture
-    uint64_t num_blocks = 100;
+    uint64_t num_blocks = 50;
 
     // allocate the memory for the samples, but do not actually init the container
     samples.reserve(num_blocks * block_size);
@@ -89,7 +105,12 @@ int main(int argc, char** argv)
 
         rv = hackrf_set_sample_rate(dev, sample_rate);
         rv |= hackrf_set_freq(dev, freq);
+        rv |= hackrf_set_vga_gain(dev, 20);
+        rv |= hackrf_set_lna_gain(dev, rx_gain);
 
+        std::cout << std::endl << "Press enter to collect." << std::endl;
+        std::cin.ignore();
+        
         // set the counter for the number of blocks to zero
         blocks_captured = 0;
 
@@ -97,12 +118,16 @@ int main(int argc, char** argv)
         rv = hackrf_start_rx(dev, rx_callback, NULL);
 
         // wait for the correct number of blocks to be collected
-        while (blocks_captured < num_blocks);
+        while (blocks_captured < num_blocks)
+        {
+            sleep_ms(10);
+        }
 
         // stop the receive callback
         rv = hackrf_stop_rx(dev);
 
         // save the samples to a file
+        std::cout << std::endl << "num samples captured: " << blocks_captured* block_size << "/" << samples.size() << std::endl;
         std::string save_filename = "../test_hackrf_save.bin";
         write_iq_data(save_filename, samples);
 
