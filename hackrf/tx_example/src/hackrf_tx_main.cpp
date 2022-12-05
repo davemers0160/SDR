d2e639ed29b61d7a7f3a49b0470749d183f0da38
Dave <davemers@iu.edu>
2022-12-04 22:13:59 -0500

    still trying to get tx working

--- hackrf/tx_example/src/hackrf_tx_main.cpp
+++ hackrf/tx_example/src/hackrf_tx_main.cpp
@@ -2,12 +2,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
-
+#include <Windows.h>
 #elif defined(__linux__)
 
 #endif
 
 #include <cstdint>
+#include <ctime>
 #include <cmath>
 #include <iostream>
 #include <sstream>
@@ -29,13 +30,30 @@ const double pi2 = 2.0 * 3.14159265358979323846;
 
 const std::complex<double> j(0, 1);
 
-const uint64_t block_size = 262144 >> 1;
-static std::vector<std::complex<int8_t>> samples;
-static std::vector<std::complex<int8_t>>::iterator samples_itr = samples.begin();
+//const uint64_t block_size = 262144 >> 1;
+//static std::vector<std::complex<int8_t>> samples;
+static std::vector<uint8_t> samples;
+
+//static std::vector<std::complex<int8_t>>::iterator samples_itr = samples.begin();
 static uint64_t blocks_captured;
 
 static uint64_t data_index;
 static bool tx_complete;
+
+
+//-----------------------------------------------------------------------------
+inline void sleep_ms(uint32_t value)
+{
+
+#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+    Sleep(value);
+#else
+    const timespec delay[] = { 0, (uint32_t)(value * 1000000) };
+    nanosleep(delay, NULL);
+#endif
+
+}   // end of sleep_ms
+
 
 //-----------------------------------------------------------------------------
 int tx_callback(hackrf_transfer* transfer)
@@ -43,10 +61,8 @@ int tx_callback(hackrf_transfer* transfer)
     size_t bytes_to_xfer = transfer->buffer_length;
     size_t bytes_remaining = samples.size() - data_index;
 
-
     // check the current index and the transfer size to see where we are at in the samples
     
-
     // start at index == 0
     // if the size of the samples buffer is larger than the transfer->buffer length then fill the transfer buffer
     // increment the index by the number of bytes_to_xsfer
@@ -56,6 +72,7 @@ int tx_callback(hackrf_transfer* transfer)
         transfer->buffer = reinterpret_cast<uint8_t*>(&samples[data_index]);
         transfer->valid_length = bytes_to_xfer;
         data_index += bytes_to_xfer;
+        return 0;
     }
     else
     {
@@ -65,13 +82,22 @@ int tx_callback(hackrf_transfer* transfer)
         transfer->valid_length = bytes_remaining;
         data_index = 0;
         tx_complete = true;
+        return 0;
     }
     
     // set the transfer valid length to the number of remaining bytes
+}   // end of tx_callback
+
+static void tx_complete_callback(hackrf_transfer* transfer, int success)
+{
+    // If a transfer failed to complete, stop the main loop.
+    if (!success) {
+        std::cout << "Error setting tx_complete_callback" << std::endl;
+        return;
+    }
 
 
-    return 0;
-}   // end of rx_callback
+}
 
 //-----------------------------------------------------------------------------
 int main(int argc, char** argv)
@@ -82,28 +108,31 @@ int main(int argc, char** argv)
     // hackrf specific structs
     hackrf_device* dev = NULL;
 
-    double sample_rate = 20000000;
-    uint64_t freq = 315000000;
-    
-    int32_t rv;
+    double sample_rate = 8000000;
+    uint64_t freq = 314500000;
+    uint32_t tx_gain = 30;
 
+    int32_t rv;
+    int bp = 1;
     uint8_t board_id = 0;
 
     // determine how many blocks to capture
     uint64_t num_blocks = 100;
 
+    std::complex<double> tmp_val;
+
     // allocate the memory for the samples, but do not actually init the container
-    samples.reserve(num_blocks * block_size);
+    //samples.reserve(num_blocks * block_size);
 
     //generate IQ samples - simple FSK
     uint32_t data = 0xAB42F58C;     // random 32-bit data
     uint32_t bit_mask = 1;
 
     // the number of samples per bit
-    uint32_t bit_samples = 20000;
+    uint32_t bit_samples = 50000;
 
     // the frequency offset for the FSK modulation - 100kHz, normalized by the sample rate
-    double freq_offset = 20000.0 / sample_rate;
+    double freq_offset = 500000.0 / sample_rate;
 
     double amplitude = 120;
 
@@ -114,7 +143,9 @@ int main(int argc, char** argv)
         {
             for (jdx = 0; jdx < bit_samples; ++jdx)
             {
-                samples.push_back(static_cast<std::complex<int8_t>>(amplitude * std::exp(j * pi2 * freq_offset * (double)jdx)));
+                tmp_val = amplitude * (std::exp(j * pi2 * freq_offset * (double)jdx) + 1.0);
+                samples.push_back((uint8_t)(tmp_val.real()));
+                samples.push_back((uint8_t)(tmp_val.imag()));
             }
         }
         // if not this is a zero
@@ -122,7 +153,9 @@ int main(int argc, char** argv)
         {
             for (jdx = 0; jdx < bit_samples; ++jdx)
             {
-                samples.push_back(static_cast<std::complex<int8_t>>(amplitude * std::exp(-j * pi2 * freq_offset * (double)jdx)));
+                tmp_val = amplitude * (std::exp(j * pi2 * (-freq_offset) * (double)jdx) + 1.0);
+                samples.push_back((uint8_t)(tmp_val.real()));
+                samples.push_back((uint8_t)(tmp_val.imag()));
             }
         }
 
@@ -130,8 +163,8 @@ int main(int argc, char** argv)
 
     }
 
-    //std::string save_filename = "../test_hackrf_save.bin";
-    //write_iq_data(save_filename, samples);
+    std::string save_filename = "../test_hackrf_save.bin";
+    write_iq_data(save_filename, samples);
 
 
     try
@@ -156,19 +189,46 @@ int main(int argc, char** argv)
 
         rv = hackrf_set_sample_rate(dev, sample_rate);
         rv |= hackrf_set_freq(dev, freq);
-
-        // set the counter for the number of blocks to zero
+        rv |= hackrf_set_txvga_gain(dev, tx_gain);
+        if (rv != HACKRF_SUCCESS)
+        {
+            fprintf(stderr, "hackrf failed: %s (%d)\n", hackrf_error_name((enum hackrf_error)rv), rv);
+            //return EXIT_FAILURE;
+        }
 
         data_index = 0;
         tx_complete = false;
 
-        // HackRF captures data in 262144 byte blocks
+        // HackRF sends data in 262144 byte blocks
         rv = hackrf_start_tx(dev, tx_callback, NULL);
+        if (rv != HACKRF_SUCCESS) 
+        {
+            fprintf(stderr, "hackrf_start_?x() failed: %s (%d)\n", hackrf_error_name((enum hackrf_error)rv), rv);
+            //return EXIT_FAILURE;
+        }
+            
+        for (idx = 0; idx < 200; ++idx)
+        {
 
-        // wait for the correct number of blocks to be collected
-        while (!tx_complete);
+            //while ((hackrf_is_streaming(dev) == HACKRF_TRUE));
+            while (!tx_complete)
+            {
+                sleep_ms(10);
+            }
 
-        // stop the receive callback
+            data_index = 0;
+            tx_complete = false;
+
+            //rv = hackrf_set_tx_block_complete_callback(dev, tx_complete_callback);
+
+
+            // wait for the correct number of blocks to be collected
+
+            // stop the transmit callback
+            //rv = hackrf_stop_tx(dev);
+            std::cout << "loop #: " << idx << std::endl;
+            bp = 0;
+        }
         rv = hackrf_stop_tx(dev);
 
         // save the samples to a file
@@ -187,4 +247,6 @@ int main(int argc, char** argv)
     rv = hackrf_close(dev); 
     rv = hackrf_exit();
     
+
+
 }   // end of main
