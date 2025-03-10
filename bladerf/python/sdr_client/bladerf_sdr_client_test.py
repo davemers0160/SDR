6c1a86dee64cedc371903bc005e539a1c7bf400c
Dave <davemers@iu.edu>
2025-03-09 20:05:39 -0400

    adding sdr server/client web app

--- bladerf/python/sdr_client/bladerf_sdr_client_test.py
+++ bladerf/python/sdr_client/bladerf_sdr_client_test.py
@@ -34,33 +34,10 @@ result = sdr_client.enable_tx(True)
 print("result: {}\n".format(result))
 
 
-# send the signal builder object to the server
-index = sb_client.create_signal_builder(sb[0])
-print("index: {}\n".format(index))
 
-result = sb_client.set_iq_savepath("d:/Projects/data/RF/")
-print("result: {}\n".format(result))
-
-# generate IQ data
-result, iq_filename = sb_client.generate_iq(index)
-print("result: {}\n".format(result))
-print("iq_filename: {}\n".format(iq_filename))
-
-# create a set of random data to generate IQ data - 20 bits per
-msg_data = np.random.randint(2, size=[sb[0].num_params, 20])
-
-# generate IQ data using the externally supplied data
-result, iq_filename = sb_client.generate_iq_with_data(msg_data, index)
-print("result: {}\n".format(result))
-print("iq_filename: {}\n".format(iq_filename))
-
-# generate N IQ data bursts
-result, iq_filename = sb_client.generate_n_iq(index, 10)
-print("result: {}\n".format(result))
-print("iq_filename: {}\n".format(iq_filename))
 
 # close the client - closes the socket and the context
-sb_client.close()
+sdr_client.close()
 
 # break point :-)
 bp = 2
---
+++ bladerf/python/sdr_web_interface/sdr_web.py
@@ -0,0 +1,86 @@
+
+import os
+
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+from flask import render_template, url_for, request, redirect
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+
+#------------------------------------------------------------------------------
+# scan directory for file
+def scan_directory_for_filetype(directory_path, file_extension):
+    matching_files = []
+
+    with os.scandir(directory_path) as entries:
+        for entry in entries:
+            if entry.is_file() and entry.name.lower().endswith(file_extension.lower()):
+                matching_files.append(entry.path)
+
+    return matching_files
+
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/test')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+
+@app.route('/test2', methods=['GET', 'POST'])
+def index():
+    iq_filename = None
+
+    file_list = scan_directory_for_filetype("D:/data/RF", "sc16")
+
+    if request.method == 'POST':
+
+        # Process the text data here
+        if request.form["submit_button"] == "b1":
+            text_data = request.form['text_input']
+        elif request.form["submit_button"] == "b2":
+            text_data = request.form['text_input2']
+        elif request.form["submit_button"] == "b3":
+            sr_str = request.form["sample_rate"]
+            start_freq_str = request.form["start_freq"]
+            stop_freq_str = request.form["stop_freq"]
+            freq_step_str = request.form["freq_step"]
+            gain_str = request.form["gain"]
+            bw_str = request.form["bandwidth"]
+            iq_filename = request.form['file_dropdown']
+
+            try:
+                sample_rate = int(sr_str)
+                start_freq = int(start_freq_str)
+                stop_freq = int(stop_freq_str)
+                freq_step = int(freq_step_str)
+                gain = int(gain_str)
+                bandwidth = int(bw_str)
+
+            except:
+                sample_rate = 20000000
+                start_freq = 1600000000
+                stop_freq = 160000000
+                freq_step = 2000000
+                gain = 65
+                bandwidth = 5000000
+
+
+        # return f"You entered: {text_data}"
+
+    # return render_template('index.html')  # Ensure you have an index.html file
+    return render_template('index.html', options=file_list) #, selected_value=text_data)
+
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(debug=True)
+
