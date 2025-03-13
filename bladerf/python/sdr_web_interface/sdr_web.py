af354acca3b602bad2022d030ce519ed6d5e4d39
Dave <davemers@iu.edu>
2025-03-12 22:06:33 -0400

    updates

--- bladerf/python/sdr_web_interface/sdr_web.py
+++ bladerf/python/sdr_web_interface/sdr_web.py
@@ -1,15 +1,26 @@
 
 import os
+import sys
 
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask
 from flask import render_template, url_for, request, redirect
 
+import numpy as np
+import numpy.typing as npt
+
+import zmq
+
+script_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(script_path + "/sdr_client/")
+from bladerf_sdr_client import bladerf_sdr_client
+
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+sdr_client = []
 
 #------------------------------------------------------------------------------
 # scan directory for file
@@ -53,6 +64,12 @@ def index():
         #     text_data = request.form['text_input']
         # elif request.form["submit_button"] == "b2":
         #     # text_data = request.form['text_input2']
+        elif request.form["submit_button"] == "Connect":
+            sdr_server_ip = request.form["sdr_server_ip"]
+            sdr_server_port = request.form["sdr_server_port"]
+
+            # create the client server
+            sdr_client = bladerf_sdr_client(sdr_server_ip, sdr_server_port)
 
         elif request.form["submit_button"] == "Config Tx":
             sr_str = request.form["sample_rate"]
@@ -64,20 +81,23 @@ def index():
             iq_filename = request.form['file_dropdown']
 
             try:
-                sample_rate = int(sr_str)
-                start_freq = int(start_freq_str)
-                stop_freq = int(stop_freq_str)
-                freq_step = int(freq_step_str)
-                gain = int(gain_str)
-                bandwidth = int(bw_str)
+                sample_rate = np.uint32(int(sr_str))
+                start_freq = np.uint64(int(start_freq_str))
+                stop_freq = np.uint64(int(stop_freq_str))
+                freq_step = np.uint32(int(freq_step_str))
+                gain = np.uint32(int(gain_str))
+                bandwidth = np.uint32(int(bw_str))
+
+                result = sdr_client.config_tx(start_freq, stop_freq, freq_step, sample_rate, bandwidth, gain)
+                print("result: {}\n".format(result))
 
             except:
-                sample_rate = 20000000
-                start_freq = 1600000000
-                stop_freq = 160000000
-                freq_step = 2000000
-                gain = 65
-                bandwidth = 5000000
+                sample_rate = np.uint32(20000000)
+                start_freq = np.uint64(1600000000)
+                stop_freq = np.uint64(160000000)
+                freq_step = np.uint32(2000000)
+                gain = np.uint32(65)
+                bandwidth = np.uint32(5000000)
 
 
         # return f"You entered: {text_data}"
