6ad768e5d45fb554420831e7ff586e5194795edd
david emerson <davemers0160@gmail.com>
2025-06-04 22:20:30 -0400

    Update sdr_web.py

--- bladerf/python/sdr_web_interface/sdr_web.py
+++ bladerf/python/sdr_web_interface/sdr_web.py
@@ -308,5 +308,5 @@ if __name__ == '__main__':
 
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
